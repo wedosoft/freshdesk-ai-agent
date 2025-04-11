b3886788af596aa85a071b068d2473430d93f025
wedosoft <alan@wedosoft.net>
2025-04-11 16:26:35 +0900

    Initial commit: Freshdesk AI Agent app structure

--- lambda/layer/python/aws_lambda_powertools/middleware_factory/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Utilities to enhance middleware
-!!! abstract "Usage Documentation"
-    [`Middleware Factory`](../utilities/middleware_factory.md)
-"""
-
-from .factory import lambda_handler_decorator
-
-__all__ = ["lambda_handler_decorator"]
